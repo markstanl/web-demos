315330190f4d999633078720673d48e445e141d5
markstanl <markgstanley1@gmail.com>
2024-03-08 14:08:26 -0600

    got rid of the awesome loader because of deployment issues

--- src/Demos/Camping Demo/Forest Components/CampingSuspense.jsx
+++ src/Demos/Camping Demo/Forest Components/CampingSuspense.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import CampingBG from "./CampingBG.jpg";
-import { BookLoader } from "react-awesome-loaders";
 
 const CampingSuspense = () => {
   return (
     <div className="w-full h-full flex justify-center items-center font-playfair ">
-      <img src={CampingBG} alt="Loading..." className="w-full h-full" />
-      <BookLoader
-        background={"linear-gradient(135deg, #311A0C, #311A0C)"}
-        className={"absolute font-playfair"}
-        shadowColor={"#131001"}
-        text={"Loading"}
-      />
+      Loading
     </div>
   );
 };
