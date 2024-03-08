c43af294cdb6689b510544b7da808a865bc98ba6
markstanl <markgstanley1@gmail.com>
2024-03-08 13:56:32 -0600

    finished the third typoe

--- src/Demos/Alriez Portfolio/Landing Page Content/TimelineModernProjects.jsx
+++ src/Demos/Alriez Portfolio/Landing Page Content/TimelineModernProjects.jsx
@@ -4,7 +4,7 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import github from "./assets/github.png";
+import github from "./Assets/github.png";
 
 const Timeline = () => {
   return (
