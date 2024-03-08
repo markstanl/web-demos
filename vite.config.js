2b5639ce951cef4defbbfc07bd85fb71b0298c11
markstanl <markgstanley1@gmail.com>
2024-03-08 14:01:10 -0600

    added react loaders to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
-})
+  assetsInclude: ["**/*.glb"],
+  build: {
+    rollupOptions: {
+      external: ["react-awesome-loaders"],
+    },
+  },
+});
